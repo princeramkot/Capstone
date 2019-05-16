00fb93cf52ced50cf2d967b67fb774706e977b8c
princeramkot <32639995+princeramkot@users.noreply.github.com>
2019-05-16 20:30:38 +0530

    Add files via upload

---
+++ dbcon.php
@@ -0,0 +1,16 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "greasemonkey";
+
+// Create connection
+$con = mysqli_connect($servername, $username, $password, $database);
+
+// Check connection
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
